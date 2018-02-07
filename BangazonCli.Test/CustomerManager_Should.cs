2ae467de68ee1264a4860a180be60d21d061ec34
Lissa Goforth <lissagoforth@gmail.com>
2018-02-07 10:15:47 -0600

    created function framework for tests

--- BangazonCli.Test/CustomerManager_Should.cs
+++ BangazonCli.Test/CustomerManager_Should.cs
@@ -1,33 +1,79 @@
 using System;
 using BangazonCli;
-// using BangazonCLI;
 using System.Collections.Generic;
 using Xunit;
 
 namespace BangazonCli.Test {
 
     public class CustomerManager_Should {
-        // [Fact]
-        // public void GetAllCustomers () {
-        //     CustomerManager manager = new CustomerManager ();
+        private Customer _customer;
+        public CustomerManager_Should() {
+            _customer = new Customer (
+                1,
+                "Chaz",
+                "Vanderbilt",
+                "Brentwood",
+                "TN",
+                "37027",
+                "615-555-1234",
+                DateTime.Now,
+                DateTime.Now
+            );
+        }
+        
 
-        //     manager.Add (_customer);
+        [Fact]
+        public void GetAllCustomers () {
 
-        //     List<Customer> allCustomers = manager.GetAllCustomers ();
+            CustomerManager manager = new CustomerManager ();
 
-        //     Assert.Contains (_customer, allCustomers);
-        // }
 
-        // [Fact]
-        // public void GetSingleCustomer () {
-        //     JobManager manager = new JobManager ();
-        //     manager.Add (_job);
-        //     Job theJob = manager.GetSingleJob (1);
+            manager.Add (_customer);
 
-        //     Assert.Equal (theJob.Id, 1);
-        //     Assert.Equal (theJob.Description, "We need cheap labor");
-        //     Assert.Equal (theJob.Title, "Junior Developer");
+            List<Customer> allCustomers = manager.GetAllCustomers ();
 
-        // } 
+            Assert.Contains (_customer, allCustomers);
+        }
+
+        [Fact]
+        public void GetSingleCustomer () {
+            CustomerManager manager = new CustomerManager ();
+            manager.Add (_customer);
+            Customer theCustomer = manager.GetSingleCustomer (1);
+
+            Assert.Equal (1, theCustomer.Id);
+            Assert.Equal ("Chaz", theCustomer.FirstName);
+            Assert.Equal ("Vanderbilt", theCustomer.LastName);
+            Assert.Equal ("Brentwood",theCustomer.City);
+            Assert.Equal ("TN", theCustomer.State);
+            Assert.Equal ("37027", theCustomer.PostalCode);
+            Assert.Equal ("615-555-1234",theCustomer.PhoneNumber);
+        } 
+        [Fact]
+        public void ActivateCustomer () {
+
+        }
+
+        [Fact]
+        public void AddCustomerPayment () {
+            
+        }
+
+        [Fact]
+        public void AddCustomerProduct () {
+
+        }
+
+        [Fact]
+        public void AddProductToOrder () {
+
+        }
+        
+        //Will this go in an OrderManager?
+        [Fact]
+        public void CompleteOrder () {
+
+        }
+
     }
 }
