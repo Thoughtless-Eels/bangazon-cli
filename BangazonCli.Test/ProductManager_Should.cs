abe2e2adbc984cb0d7ec4c868a855ab0a226e905
John Dulaney <john.s.dulaney1@gmail.com>
2018-02-08 09:56:00 -0600

    working on productmanage should

--- BangazonCli.Test/ProductManager_Should.cs
+++ BangazonCli.Test/ProductManager_Should.cs
@@ -6,21 +6,66 @@ using Xunit;
 namespace BangazonCli.Test {
     public class ProductManager_Should {
         private Product _product;
+        private Customer _customer;
         private DateTime dt = DateTime.Now;
-   
+
+        public void productConstructor () {
+            _product = new Product (
+                1,
+                4,
+                12.55,
+                "Book",
+                "A Handcrafted book about See Sherp",
+                2,
+                72,
+                dt
+            );
+        }
+
         [Fact]
         public void CreateProduct () {
             // Assert.Contains (_product.Name, "Book");
         }
 
         [Fact]
+        public void ListAllProducts () {
+            ProductManager productmanager = new ProductManager ();
+            productmanager.Add (_product);
+            List<Product> listProduct = productmanager.GetAllProducts ();
+            Assert.Contains (_product, listProduct);
+        }
+
+        [Fact]
+        public void RemoveProduct () {
+            ProductManager productmanager = new ProductManager ();
+            productmanager.Add (_product);
+            List<Product> listProduct = productmanager.GetAllProducts ();
+
+            productmanager.RemoveSingleProduct (_product);
+
+            Assert.DoesNotContain (_product, listProduct);
+        }
+
+        [Fact]
+        public void UpdateProduct () {
+            ProductManager productmanager = new ProductManager ();
+            productmanager.Add (_product);
+            List<Product> listProduct = productmanager.GetAllProducts ();
+
+            productmanager.UpdateSingleProduct (_product);
+
+            Assert.Equal (_product.ProductId, 1);
+        }
+
+        [Fact]
         public void AddProductToCustomer () {
-            // Need to pull the instance of _product and _customer together
 
-            // Assert that the table contains CustomerId on product
-            
+            // Pull the instance of _product and _customer together
 
+            // Add the Customer.Id Key to the product. - .Add
 
+            // Assert that the table contains CustomerId on product - Assert.
+            Assert.Contains (_product.CustomerId, 4);
         }
 
         [Fact]
--- BangazonCli/Managers/ProductManager.cs
+++ BangazonCli/Managers/ProductManager.cs
@@ -1,32 +1,32 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace BangazonCli
-{
-    public class ProductManager
-    {
-        private List<Product> _productTable = new List<Product>();
+namespace BangazonCli {
+    public class ProductManager {
+        private List<Product> _productTable = new List<Product> ();
 
-        public void Add(Product ToothBrush)
-        {
-            _productTable.Add(ToothBrush);
+        public void Add (Product product) {
+            _productTable.Add (product);
         }
-        
-        public List<Product> GetAllProducts ()
-        {
+
+        public List<Product> GetAllProducts () {
             return _productTable;
         }
 
-        public Product GetSingleProduct(int id)
-        {
-            return _productTable.Where(p => p.Id == id).Single();
+        public Product GetSingleProduct (int id) {
+            return _productTable.Where (p => p.Id == id).Single ();
         }
 
+        public void RemoveSingleProduct (Product product) {
+            _productTable.Remove (product);
+        }
 
-
-        public void UpdatePrice(Product product, double price)
-        {
+        public void UpdatePrice (Product product, double price) {
             product.Price = price;
+        }
+        
+        public void UpdateSingleProduct (Product product) {
+            product.ProductId = ProductId;
         }
     }
 }
